8ffe217c5b8137e99bad7a9043d794bea07552e1
Your Name <you@example.com>
2023-05-24 14:01:28 +0500

    fix

--- configure-db.sh
+++ configure-db.sh
@@ -10,7 +10,7 @@ ERRCODE=1
 i=0
 
 while [[ $DBSTATUS -ne 0 ]] && [[ $i -lt 60 ]] && [[ $ERRCODE -ne 0 ]]; do
-	i=$i+1
+	i=$((i+1))
 	DBSTATUS=$(/opt/mssql-tools/bin/sqlcmd -h -1 -t 1 -U sa -P $MSSQL_SA_PASSWORD -Q "SET NOCOUNT ON; Select SUM(state) from sys.databases")
 	ERRCODE=$?
 	sleep 1
