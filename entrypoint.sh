435ca8e0c9c39f42e313364a413ee800dcc67d6a
Your Name <you@example.com>
2023-05-28 20:51:03 +0500

    scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 
 # Start the script to create the DB and user
-#/usr/config/configure-db.sh &
+/usr/config/configure-db.sh &
 
 # Start SQL Server
-#/opt/mssql/bin/sqlservr
+/opt/mssql/bin/sqlservr
